c3effddfd2cda5a96306fc9c7b87ec61a82f8817
vellasco <gabrielvellasco@id.uff.br>
2022-03-23 00:06:01 -0300

    forms and some tests

--- BlazorTest/obj/Debug/net5.0/RazorDeclaration/wwwroot/css/FormComponent.razor.g.cs
+++ BlazorTest/obj/Debug/net5.0/RazorDeclaration/wwwroot/css/FormComponent.razor.g.cs
@@ -4,7 +4,7 @@
 #pragma warning disable 0649
 #pragma warning disable 0169
 
-namespace BlazorTest.Shared
+namespace BlazorTest.wwwroot.css
 {
     #line hidden
     using System;
@@ -82,28 +82,13 @@ using BlazorTest.Shared;
 #line default
 #line hidden
 #nullable disable
-    public partial class NavMenu : Microsoft.AspNetCore.Components.ComponentBase
+    public partial class FormComponent : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
         protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
         {
         }
         #pragma warning restore 1998
-#nullable restore
-#line 33 "C:\Users\gabri\source\repos\BlazorTest\BlazorTest\Shared\NavMenu.razor"
-       
-    private bool collapseNavMenu = true;
-
-    private string NavMenuCssClass => collapseNavMenu ? "collapse" : null;
-
-    private void ToggleNavMenu()
-    {
-        collapseNavMenu = !collapseNavMenu;
-    }
-
-#line default
-#line hidden
-#nullable disable
     }
 }
 #pragma warning restore 1591
